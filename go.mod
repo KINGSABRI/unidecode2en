7a2249e83c7a3fa1e85ee0dd6f0386574edcefdc
KINGSABRI <king.sabri@gmail.com>
2022-12-26 02:20:10 +0300

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/KINGSABRI/unidecode2en
+
+go 1.19
+
+require github.com/mozillazg/go-unidecode v0.2.0
---
+++ unidecode2en.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/mozillazg/go-unidecode"
+)
+
+func check(e error) {
+	if e != nil {
+		fmt.Printf("Error: %v", e)
+	}
+}
+
+func main() {
+
+	var (
+		filePtr string
+		strPtr  string
+	)
+
+	flag.StringVar(&filePtr, "file", "", "a file with unicode to decode into english")
+	flag.StringVar(&filePtr, "f", "", "a file with unicode to decode into english")
+	flag.StringVar(&strPtr, "string", "", "a string with unicode to decode into english")
+	flag.StringVar(&strPtr, "s", "", "a string with unicode to decode into english")
+	flag.Parse()
+
+	if len(filePtr) == 0 && len(strPtr) == 0 {
+		fmt.Println("Usage: ")
+		fmt.Println("unidecode2en -file <FILE>")
+		fmt.Println("unidecode2en -string <STRING>")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if filePtr != "" {
+		file, err := os.ReadFile(filePtr)
+		// check(err)
+		if err != nil {
+			fmt.Printf("Error: %v", err)
+			os.Exit(0)
+		}
+		fmt.Print(unidecode.Unidecode(string(file)))
+	}
+
+	if strPtr != "" {
+		fmt.Print(unidecode.Unidecode(strPtr))
+	}
+
+}
